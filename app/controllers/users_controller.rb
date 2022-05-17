00facdba05a5ef25a43b0b999a68952abd43c968
aman <amangoad546@gmail.com>
2022-05-17 16:47:06 +0530

    yes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, only: [:show, :edit, :update]
+
 def show
-@user= User.find(params[:id])
+#@user= User.find(params[:id])
 @articles = @user.articles
 end
 
@@ -13,16 +15,17 @@ end
     end
 
     def update  
-       @user = User.find(params[:id])   
+       #@user = User.find(params[:id])   
     end
     def edit
-        @user = User.find(params[:id])
+        #@user = User.find(params[:id])
     end
 
     def create
         @user = User.new(user_params)
 
         if @user.save 
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to the Star App ,#{@user.username} you have successfully signed up"
             redirect_to articles_path
            
@@ -35,4 +38,8 @@ end
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+
+    def set_user
+        @user = User.find(params[:id])
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  resources :create_users
-  resources :users
   root 'pages#home'
   #root 'articles#index'
-  get '/articles', to: 'articles#index'
-
-
-  get 'signup', to: 'users#new'
-  #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  #get '/articles', to: 'articles#index'
+ get 'about', to: 'pages#about'
   resources :articles
-
-  post 'users', to: 'users#create'
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  get 'signup', to: 'users#new'
   resources :users, except: [:new ]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
 end
